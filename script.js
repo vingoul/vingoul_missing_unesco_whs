0edc47c5fafd38899217072b16372edcea2096d6
vingoul <127652340+vingoul@users.noreply.github.com>
2025-02-24 22:18:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,22 +10,108 @@ var json = [
                    "AL-04 Ancient and Primeval Beech Forests of the Carpathians and Other Regions of Europe (2007)"]
     },
     {
-        "type": "MOTW",
-        "values": ["Austria", "Costa Rica", "Canada", "UK", "Hong Kong", "Italy", "Finland", "Norway", "Spain", "Malaysia", "Japan", "Germany", "Czech Republic", "Taiwan", "China", "England", "Croatia", "Thailand", "UAE", "Sovereign Military Order Of Malta", "Poland", "Ukraine", "Armenia", "Portgual", "Latvia", "Turkey (Arranged)", "St Pierre and Miquelon", "Singapore", "Malta", "India (Arranged)", "Nepal", "Maldives", "Philippines", "Switzerland", "Slovenia", "Peru", "Belarus", "Bahamas", "Greece", "Hungary", "Ireland", "Cyprus", "Indonesia", "Pitcairn Islands", "San Marino", "Greenland", "Estonia", "Honduras", "Mozambique", "Denmark", "New Zealand ", "Israel", "USA (Arranged)", "France (Arranged)", "Nauru (Arranged)", "Mexico", "Egypt", "Macau"]
+        "type": "Algeria",
+        "values": ["DZ-01 Al Qal'a of Beni Hammad (1980)", 
+                   "DZ-02 Djémila (1982)", 
+                   "DZ-03 M'Zab Valley (1982)", 
+                   "DZ-04 Tassili n'Ajjer (1982)", 
+                   "DZ-05 Timgad (1982)", 
+                   "DZ-06 Tipasa (1982)", 
+                   "DZ-07 Kasbah of Algiers (1993)"]
 
     },
     {
-        "type": "Heritage Prints Indian States",
-        "values": ["Karnataka", "Punjab"]
+        "type": "Andorra",
+        "values": ["AD-01 Madriu-Perafita-Claror Valley (2004)"]
+
     },
     {
-        "type": "Loupaper",
-        "values": ["Alaska", "California", "Colorado", "Connecticut", "Florida", "Georgia", "Kentucky", "Louisiana", "Maine", "Maryland", "Michigan", "Minnesota", "New Jersey", "New Mexico", "New York", "Ohio", "Pennsylvania", "South Carolina", "Tennessee", "Texas", "Washington"]
+        "type": "Angola",
+        "values": ["AO-01 Mbanza Kongo, Vestiges of the Capital of the former Kingdom of Kongo (2017)"]
+
+    },
+    {
+        "type": "Antigua and Barbuda",
+        "values": ["AG-01 Antigua Naval Dockyard and Related Archaeological Sites (2016)"]
+
+    },
+    {
+        "type": "Argentina",
+        "values": ["AR-01 Los Glaciares National Park (1981)", 
+                   "AR-02 Jesuit Missions of the Guaranis: San Ignacio Mini, Santa Ana, Nuestra Señora de Loreto and Santa Maria Mayor (Argentina), Ruins of Sao Miguel das Missoes (Brazil) (1983)", 
+                   "AR-03 Iguazu National Park (1984)", 
+                   "AR-04 Cueva de las Manos, Río Pinturas (1999)", 
+                   "AR-05 Península Valdés (1999)", 
+                   "AR-06 Ischigualasto / Talampaya Natural Parks (2000)", 
+                   "AR-07 Jesuit Block and Estancias of Córdoba (2000)", 
+                   "AR-08 Quebrada de Humahuaca (2003)", 
+                   "AR-09 Qhapaq Ñan, Andean Road System (2014)", 
+                   "AR-10 The Architectural Work of Le Corbusier, an Outstanding Contribution to the Modern Movement (2016)", 
+                   "AR-11 Los Alerces National Park (2017)", "AR-12 ESMA Museum and Site of Memory – Former Clandestine Center of Detention, Torture and Extermination (2023)"]
+
+    },
+    {
+        "type": "Armenia",
+        "values": ["AM-01 Monasteries of Haghpat and Sanahin (1996)", 
+                   "AM-02 Cathedral and Churches of Echmiatsin and the Archaeological Site of Zvartnots (2000)", 
+                   "AM-03 Monastery of Geghard and the Upper Azat Valley (2000)"]
+    },        
+    {
+        "type": "Australia",
+        "values": ["AU-01 Great Barrier Reef (1981)", 
+                   "AU-02 Kakadu National Park (1981)", 
+                   "AU-03 Willandra Lakes Region (1981)", 
+                   "AU-04 Lord Howe Island Group (1982)", 
+                   "AU-05 Tasmanian Wilderness (1982)", 
+                   "AU-06 Gondwana Rainforests of Australia (1986)", "AU-07 Uluru-Kata Tjuta National Park (1987)", 
+                   "AU-08 Wet Tropics of Queensland (1988)", 
+                   "AU-09 Shark Bay, Western Australia (1991)", "AU-10 K’gari (Fraser Island) (1992)
+                   "AU-11 Australian Fossil Mammal Sites (Riversleigh / Naracoorte) (1994)", 
+                   "AU-12 Heard and McDonald Islands (1997)", 
+                   "AU-13 Macquarie Island (1997)", 
+                   "AU-14 Greater Blue Mountains Area (2000)", 
+                   "AU-15 Purnululu National Park (2003)", 
+                   "AU-16 Royal Exhibition Building and Carlton Gardens (2004)", 
+                   "AU-17 Sydney Opera House (2007)", 
+                   "AU-18 Australian Convict Sites (2010)", 
+                   "AU-19 Ningaloo Coast (2011)", 
+                   "AU-20 Budj Bim Cultural Landscape (2019)"]
+    },
+    {
+        "type": "Austria",
+        "values": ["AT-01 Historic Centre of the City of Salzburg (1996)", 
+                   "AT-02 Palace and Gardens of Schönbrunn (1996)", 
+                   "AT-03 Hallstatt-Dachstein / Salzkammergut Cultural Landscape (1997)", 
+                   "AT-04 Semmering Railway (1998)", 
+                   "AT-05 City of Graz – Historic Centre and Schloss Eggenberg (1999)", 
+                   "AT-06 Wachau Cultural Landscape (2000)", 
+                   "AT-07 Historic Centre of Vienna (2001)", 
+                   "AT-08 Fertö / Neusiedlersee Cultural Landscape (2001)", 
+                   "AT-09 Ancient and Primeval Beech Forests of the Carpathians and Other Regions of Europe (2007)", 
+                   "AT-10 Prehistoric Pile dwellings around the Alps (2011)", 
+                   "AT-11 Frontiers of the Roman Empire – The Danube Limes (Western Segment) (2021)", 
+                   "AT-12 The Great Spa Towns of Europe (2021)"]
     },
      {
-        "type": "wc",
-        "values": ["blr", "California", "Colorado", "Connecticut", "Florida", "Georgia", "Kentucky", "Louisiana", "Maine", "Maryland", "Michigan", "Minnesota", "New Jersey", "New Mexico", "New York", "Ohio", "Pennsylvania", "South Carolina", "Tennessee", "Texas", "Washington"]
-    }
+        "type": "Azerbaijan",
+        "values": ["AZ-01 Walled City of Baku with the Shirvanshah's Palace and Maiden Tower (2000)", 
+                   "AZ-02 Gobustan Rock Art Cultural Landscape (2007)", 
+                   "AZ-03 Historic Centre of Sheki with the Khan’s Palace (2019)", 
+                   "AZ-04 Hyrcanian Forests (2023)", 
+                   "AZ-05 Cultural Landscape of Khinalig People and “Köç Yolu” Transhumance Route (2023)"]
+    },
+    {
+        "type": "Heritage",
+        "values": ["Karnataka"]
+    },
+    {
+        "type": "Heritage",
+        "values": ["Karnataka"]
+    },
+    {
+        "type": "Heritage",
+        "values": ["Karnataka"]
+    },
 ]
 
 
